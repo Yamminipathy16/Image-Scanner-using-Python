a79dbc20fbe130eae13e5b03e9fe7dca0cae53dd
Yamminipathy16 <69075739+Yamminipathy16@users.noreply.github.com>
2020-08-01 16:02:02 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,21 +1,21 @@
 from cv2 import cv2
 import numpy as np
 import mapper
-image=cv2.imread("t4.jpg")  #read in the image
-image=cv2.resize(image,(1366,768)) #resizing because opencv does not work well with bigger images
+image=cv2.imread("t4.jpg")  #upload files only in jpg format
+image=cv2.resize(image,(1366,768)) #resize to your PC's screen size for better image quality
 orig=image.copy()
 
 gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)  #RGB To Gray Scale
 cv2.imshow("Title",gray)
 
-blurred=cv2.GaussianBlur(gray,(5,5),0)  #(5,5) is the kernel size and 0 is sigma that determines the amount of blur
-cv2.imshow("Blur",blurred)
+blurred=cv2.GaussianBlur(gray,(5,5),0)  #Blurred image
+#cv2.imshow("Blur",blurred)
 
 edged=cv2.Canny(blurred,30,50)  #30 MinThreshold and 50 is the MaxThreshold
-cv2.imshow("Canny",edged)
+#cv2.imshow("Canny",edged)
 
 
-contours,hierarchy=cv2.findContours(edged,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)  #retrieve the contours as a list, with simple apprximation model
+contours,hierarchy=cv2.findContours(edged,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)  
 contours=sorted(contours,key=cv2.contourArea,reverse=True)
 
 #the loop extracts the boundary contours of the page
@@ -30,7 +30,7 @@ approx=mapper.mapp(target) #find endpoints of the sheet
 
 pts=np.float32([[0,0],[800,0],[800,800],[0,800]])  #map to 800*800 target window
 
-op=cv2.getPerspectiveTransform(approx,pts)  #get the top or bird eye view effect
+op=cv2.getPerspectiveTransform(approx,pts)  
 dst=cv2.warpPerspective(orig,op,(800,800))
 
 
